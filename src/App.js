994c29f73ae9ab7d0ce418aa0bccf0eae536944e
Corinne-Coding <pradier.corinne@gmail.com>
2021-04-13 15:28:42 +0200

    new design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import { useState } from "react";
 
 // Components
-import ToggleButton from "./components/ToggleButton/ToggleButton";
-import Link from "./components/Link/Link";
 import PartOne from "./components/PartOne/PartOne";
-
-// Icons
-import logoEnglish from "./img/logo-en.svg";
-import logoFrench from "./img/logo-fr.svg";
 
 // Data
 import json from "./data.json";
@@ -18,26 +12,9 @@ function App() {
   const [language, setLanguage] = useState(true);
 
   return (
-    <div className="app">
-      <header>
-        <img src={language ? logoEnglish : logoFrench} alt="logo" />
-
-        <div>
-          <ul>
-            {language
-              ? data.english.header.map((item, index) => {
-                  return <Link key={index} text={item.text} icon={item.icon} />;
-                })
-              : data.french.header.map((item, index) => {
-                  return <Link key={index} text={item.text} icon={item.icon} />;
-                })}
-          </ul>
-          <ToggleButton language={language} setLanguage={setLanguage} />
-        </div>
-      </header>
-
+    <>
       <PartOne data={data} language={language} />
-    </div>
+    </>
   );
 }
 
--- src/components/PartOne/PartOne.js
+++ src/components/PartOne/PartOne.js
@@ -4,62 +4,61 @@ import "./PartOne.scss";
 // Particles
 import Particles from "react-particles-js";
 
-// import portrait from "../../img/portrait.png";
-
 const Part = ({ data, language }) => {
   return (
-    <section className="part-one">
-      <Particles
-        className="particles"
-        params={{
-          particles: {
-            number: {
-              value: 300,
-              density: {
-                enable: true,
-                value_area: 1500,
+    <div className="portrait">
+      <section className="part-one">
+        {
+          <Particles
+            className="particles"
+            params={{
+              particles: {
+                number: {
+                  value: 400,
+                  density: {
+                    enable: true,
+                    value_area: 1500,
+                  },
+                },
+                line_linked: {
+                  enable: true,
+                  opacity: 0.02,
+                },
+                move: {
+                  speed: 0.05,
+                },
+                size: {
+                  value: 1,
+                },
+                opacity: {
+                  anim: {
+                    enable: true,
+                    speed: 1,
+                    opacity_min: 0.05,
+                  },
+                },
               },
-            },
-            line_linked: {
-              enable: true,
-              opacity: 0.02,
-            },
-            move: {
-              direction: "left",
-              speed: 0.05,
-            },
-            size: {
-              value: 1,
-            },
-            opacity: {
-              anim: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0.05,
-              },
-            },
-          },
-          interactivity: {
-            events: {
-              onclick: {
-                enable: true,
-                mode: "push",
-              },
-            },
-            modes: {
-              push: {
-                particles_nb: 1,
-              },
-            },
-          },
-          retina_detect: true,
-        }}
-      />
-      <div className="titles">
-        <h1>Corinne Pradier</h1>
-        <h2>{language ? data.english.mainTitle : data.french.mainTitle}</h2>
-      </div>
-    </section>
+
+              retina_detect: true,
+            }}
+          />
+        }
+        <div className="titles">
+          <h1>Corinne Pradier</h1>
+          <h2>{language ? data.english.mainTitle : data.french.mainTitle}</h2>
+        </div>
+
+        <div className="menu">
+          {language
+            ? data.english.header.map((item, index) => {
+                return <h3 key={index}>{item.text}</h3>;
+              })
+            : data.french.header.map((item, index) => {
+                return <h3 key={index}>{item.text}</h3>;
+              })}
+        </div>
+      </section>
+    </div>
   );
 };
 
