e058356cdc05c76369fccfa7dc79fcd02b5f65d3
Corinne-Coding <pradier.corinne@gmail.com>
2021-04-18 11:29:12 +0200

    add new animation on name + add snapping scroll

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import json from "./data.json";
 import Particles from "react-particles-js";
 
 // Components
+
 import Flag from "./components/Flag/Flag";
 import AnimatedArrow from "./components/AnimatedArrow/AnimatedArrow";
 
@@ -17,76 +18,126 @@ function App() {
   const [data] = useState(json);
   // Languages : true = english / false = french
   const [language, setLanguage] = useState(true);
+  const [content, setContent] = useState("home");
 
   return (
     <>
-      <section className="app">
-        {
-          <Particles
-            className="particles"
-            params={{
-              particles: {
-                number: {
-                  value: 200,
-                  density: {
+      <div className="scroll-snap-container">
+        <section className="home snap-element">
+          {
+            <Particles
+              className="particles"
+              params={{
+                particles: {
+                  number: {
+                    value: 200,
+                    density: {
+                      enable: true,
+                      value_area: 1500,
+                    },
+                  },
+                  line_linked: {
                     enable: true,
-                    value_area: 1500,
+                    opacity: 0.04,
+                  },
+                  move: {
+                    speed: 0.05,
+                  },
+                  size: {
+                    value: 1,
+                  },
+                  opacity: {
+                    anim: {
+                      enable: true,
+                      speed: 1,
+                      opacity_min: 0.05,
+                    },
                   },
                 },
-                line_linked: {
-                  enable: true,
-                  opacity: 0.04,
-                },
-                move: {
-                  speed: 0.05,
-                },
-                size: {
-                  value: 1,
-                },
-                opacity: {
-                  anim: {
-                    enable: true,
-                    speed: 1,
-                    opacity_min: 0.05,
-                  },
-                },
-              },
 
-              retina_detect: true,
-            }}
-          />
-        }
+                retina_detect: true,
+              }}
+            />
+          }
 
-        <div className="languages">
-          <Flag flag="en" setLanguage={setLanguage} language={language} />
-          <Flag flag="fr" setLanguage={setLanguage} language={language} />
-        </div>
+          <div className="languages">
+            <Flag flag="en" setLanguage={setLanguage} language={language} />
+            <Flag flag="fr" setLanguage={setLanguage} language={language} />
+          </div>
 
-        <div className="titles">
-          <h1>Corinne Pradier</h1>
-          <h2>{language ? data.english.mainTitle : data.french.mainTitle}</h2>
-        </div>
+          <section>
+            {content === "home" ? (
+              <div className="titles">
+                <h1>
+                  <span>C</span>
+                  <span>o</span>
+                  <span>r</span>
+                  <span>i</span>
+                  <span>n</span>
+                  <span>n</span>
+                  <span>e&nbsp;</span>
+                  <span>P</span>
+                  <span>r</span>
+                  <span>a</span>
+                  <span>d</span>
+                  <span>i</span>
+                  <span>e</span>
+                  <span>r</span>
+                </h1>
+                <h2>
+                  {language ? data.english.mainTitle : data.french.mainTitle}
+                </h2>
+              </div>
+            ) : null}
+          </section>
 
-        <div className="menu">
-          {language
-            ? data.english.header.map((item, index) => {
-                return (
-                  <a href="#">
-                    <h3 key={index}>{item.text}</h3>
-                  </a>
-                );
-              })
-            : data.french.header.map((item, index) => {
-                return (
-                  <a href="#">
-                    <h3 key={index}>{item.text}</h3>
-                  </a>
-                );
-              })}
-        </div>
+          <div className="menu">
+            {language
+              ? data.english.header.map((item, index) => {
+                  return (
+                    <a href="#" key={index}>
+                      {item.text}
+                    </a>
+                  );
+                })
+              : data.french.header.map((item, index) => {
+                  return (
+                    <a href="#" key={index}>
+                      {item.text}
+                    </a>
+                  );
+                })}
+          </div>
 
-        <AnimatedArrow />
-      </section>
+          <AnimatedArrow />
+        </section>
+
+        <section
+          className="project snap-element"
+          style={{ "background-color": "deeppink" }}
+        >
+          coucou
+        </section>
+        <section
+          className="project snap-element"
+          style={{ "background-color": "pink" }}
+        >
+          coucou
+        </section>
+        <section
+          className="project snap-element"
+          style={{ "background-color": "hotpink" }}
+        >
+          coucou
+        </section>
+
+        <section
+          className="project snap-element"
+          style={{ "background-color": "orange" }}
+        >
+          coucou
+        </section>
+      </div>
     </>
   );
 }
